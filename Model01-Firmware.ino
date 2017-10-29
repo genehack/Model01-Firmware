3bd18a72e3360c9b3aa05e9e01c7390b75f9c6dd
John SJ Anderson <john@genehack.org>
2017-10-28 18:29:32 -0700

    more whitespace alignment

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -129,21 +129,20 @@ enum { DVORAK, FUNCTION, NUMPAD }; // layers
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [DVORAK] = KEYMAP_STACKED
-  (___,             Key_1,         Key_2,     Key_3,      Key_4, Key_5, Key_LEDEffectNext, // R1
-   Key_Tab,         Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, Key_Backtick,      // R2
-   Key_LeftControl, Key_A,         Key_O,     Key_E,      Key_U, Key_I,                    // R3
-   Key_PageDown,    Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X, Key_Escape,        // R4
+  (___,             Key_1,           Key_2,       Key_3,         Key_4, Key_5,     Key_LEDEffectNext, // R1
+   Key_Tab,         Key_Quote,       Key_Comma,   Key_Period,    Key_P, Key_Y,     Key_Backtick,      // R2
+   Key_LeftControl, Key_A,           Key_O,       Key_E,         Key_U, Key_I,                        // R3
+   Key_PageDown,    Key_Semicolon,   Key_Q,       Key_J,         Key_K, Key_X,     Key_Escape,        // R4
 
-   Key_Spacebar, Key_LeftControl, Key_LeftGui, Key_LeftShift,                              // arc
+   Key_Spacebar,    Key_LeftControl, Key_LeftGui, Key_LeftShift,                                      // arc
    LT(FUNCTION, Backspace),
 
-   M(MACRO_ANY),   Key_6, Key_7, Key_8, Key_9,     Key_0, Key_KeypadNumLock,               // R1
-   Key_Enter,      Key_F, Key_G, Key_C, Key_R,     Key_L, Key_Slash,                       // R2
-   Key_D, Key_H,   Key_T, Key_N, Key_S, Key_Minus,                                         // R3
-   Key_RightAlt,   Key_B, Key_M, Key_W, Key_V,     Key_Z, Key_Equals,                      // R4
+   M(MACRO_ANY),    Key_6,           Key_7,       Key_8,         Key_9, Key_0,     Key_KeypadNumLock, // R1
+   Key_Enter,       Key_F,           Key_G,       Key_C,         Key_R, Key_L,     Key_Slash,         // R2
+   Key_D,           Key_H,           Key_T,       Key_N,         Key_S, Key_Minus,                    // R3
+   Key_RightAlt,    Key_B,           Key_M,       Key_W,         Key_V, Key_Z,     Key_Equals,        // R4
 
-
-   Key_RightShift, Key_LeftAlt, Key_RightControl, Key_Spacebar,                            // arc
+   Key_RightShift, Key_LeftAlt, Key_RightControl, Key_Spacebar,                                       // arc
    LT(FUNCTION, Enter)),
 
   [FUNCTION] =  KEYMAP_STACKED
