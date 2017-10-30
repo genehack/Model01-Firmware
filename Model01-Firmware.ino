695caf8167b8115909e91df428e2eda21cd9046a
John SJ Anderson <john@genehack.org>
2017-10-29 21:14:52 -0700

    flesh out the left and right ignore lists for ModRight

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -269,8 +269,22 @@ void setup() {
   Kaleidoscope.use(&ModRight, &SpaceCadet, &DualUse);
 
   // FIXME
-  static const Key require_right[] PROGMEM = { Key_A, Key_NoKey };
-  static const Key require_left[] PROGMEM  = { Key_M, Key_NoKey };
+  static const Key require_right[] PROGMEM =
+    {
+     Key_1,         Key_2,     Key_3,      Key_4, Key_5,
+     Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y,
+     Key_A,         Key_O,     Key_E,      Key_U, Key_I,
+     Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X,
+     Key_NoKey
+    };
+  static const Key require_left[] PROGMEM =
+    {
+     Key_6, Key_7, Key_8, Key_9, Key_0, // numlock
+     Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slash,
+     Key_D, Key_H, Key_T, Key_N, Key_S, Key_Minus,
+     Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
+     Key_NoKey
+  };
 
   ModRight.require_left = require_left;
   ModRight.require_right = require_right;
